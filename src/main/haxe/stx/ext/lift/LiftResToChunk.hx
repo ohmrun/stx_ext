91bbc8653443c394d017f344d69028daee98b7f1
dayz <polysemantic@gmail.com>
2020-09-15 10:01:07 +0200

    LiftResToChunk

---
+++ src/main/haxe/stx/ext/lift/LiftResToChunk.hx
@@ -0,0 +1,10 @@
+package stx.ext.lift;
+
+class LiftResToChunk{
+  static public function toChunk<O,E>(self:Res<O,E>):Chunk<O,E>{
+    return self.fold(
+      (o) -> Val(o),
+      (e) -> End(e)
+    );
+  }
+}
